5324268829e421a4230dc78de286f0902c3969e4
Andrew Humphrey <andrew.humphrey@envato.com>
2021-06-22 12:57:44 +1200

    iamy 3.0.0+envato
    Closes #3.
    
    Signed-off-by: Andrew Humphrey <520641+andrewjhumphrey@users.noreply.github.com>

--- Formula/iamy.rb
+++ Formula/iamy.rb
@@ -1,16 +1,16 @@
 class Iamy < Formula
   desc "Envato fork of 99Designs IAMy IAM Object and S3 Bucket Policy Management tool"
   homepage "https://github.com/envato/iamy"
-  url "https://github.com/envato/iamy/archive/refs/tags/v2.5.0.tar.gz"
-  version "2.5.0+envato"
-  sha256 "68dc6a9aa9c5635c7005e203f2dd3f81ef14dce597a39305c390012b1f973ac7"
+  url "https://github.com/envato/iamy/archive/refs/tags/v3.0.0.tar.gz"
+  version "3.0.0+envato"
+  sha256 "b8ceb153b3a92eb8e9382c360d9a91e9cfb975090d3ed83b0ba9af9746fea12b"
   license "MIT"
 
   bottle do
     root_url "https://github.com/envato/homebrew-envato-iamy/releases/download/iamy-2.5.0+envato"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a4e090d42fe76ae51af47c27dad17fee581d3cb974f0fcd11c8796d7045b371"
     sha256 cellar: :any_skip_relocation, catalina:       "22ee5111191b2ad1951c5624e306e52ec455fe221d7024f310a3813dfd5b9241"
     sha256 cellar: :any_skip_relocation, big_sur:        "07f196ec85f28f15e8e471dcbe6809f5c3f6987f4494ad35c9843e5c3e64dce8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "5a4e090d42fe76ae51af47c27dad17fee581d3cb974f0fcd11c8796d7045b371"
   end
 
   depends_on "go" => :build
