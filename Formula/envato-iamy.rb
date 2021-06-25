67211812bea5f1342f16eace3f66d639a83addd3
Liam Dawson <liam.dawson@envato.com>
2021-06-25 15:36:09 +1000

    Rename iamy -> envato-iamy

--- Formula/envato-iamy.rb
+++ Formula/envato-iamy.rb
@@ -1,19 +1,15 @@
-class Iamy < Formula
+class EnvatoIamy < Formula
   desc "Envato fork of 99Designs IAMy IAM Object and S3 Bucket Policy Management tool"
   homepage "https://github.com/envato/iamy"
   url "https://github.com/envato/iamy/archive/refs/tags/v3.0.0.tar.gz"
-  version "3.0.0+envato"
+  version "3.0.0"
   sha256 "b8ceb153b3a92eb8e9382c360d9a91e9cfb975090d3ed83b0ba9af9746fea12b"
   license "MIT"
 
-  bottle do
-    root_url "https://github.com/envato/homebrew-envato-iamy/releases/download/iamy-3.0.0+envato"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "606916f8445dd428fb88a81ba7aa0a6153e9a25698a191675b7c449e038d43db"
-    sha256 cellar: :any_skip_relocation, catalina:      "76938fbaa764d38436f1b2c210cf4fac3161ab48df6adab92462c590025d900d"
-  end
-
   depends_on "go" => :build
   depends_on "awscli"
+
+  conflicts_with "iamy", because: "envato-iamy also ships an iamy binary"
 
   def install
     ENV["CGO_CFLAGS"] = "-mmacosx-version-min=10.15"
