2ba383820f783dfad8aac889ed23bce05bc9e392
Andrew Humphrey <andrew.humphrey@envato.com>
2022-05-10 14:23:32 +1200

    Add arm64 bottle

--- Formula/iamy.rb
+++ Formula/iamy.rb
@@ -8,6 +8,7 @@ class Iamy < Formula
 
   bottle do
     root_url "https://github.com/envato/homebrew-envato-iamy/releases/download/iamy-4.1.0+envato"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "60077750ef0d1345a8617f64fdb654b11049726f6ddc2464f9b866b2dae085e6"
     sha256 cellar: :any_skip_relocation, big_sur: "2547da7d1002dc28c7647c396c1f685d110e7ec93bde95c34527c49fa2cdf7ff"
   end
 
